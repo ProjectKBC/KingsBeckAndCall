92edb63198b3f374fcf40923109b6009663f14de
flanny7 <mottei.senshu@gmail.com>
2018-05-30 18:49:50 +0900

    Actor抽象クラスとCachedActor抽象クラスの定義 CachedはマニアックスのObjectPoolクラスをActorシステムに対応させたものを使うことを想定している。

---
+++ Assets/Scripts/PlayerActor.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+
+namespace Ria
+{
+    public enum PlayerID
+    {
+        Player1,
+        Player2,
+    }
+
+    public class PlayerActor : Actor
+    {
+        #region Member
+        private PlayerID playerID;
+        private PlayerScriptableObject scriptObj_;
+        private SpriteRenderer sr_ = null;
+        #endregion
+
+        #region Main Function
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="_playerID">player1 or player2</param>
+        /// <param name="_sto">PlayerScriptableObject</param>
+        /// <param name="_name">GameObjectの名前</param>
+        /// <param name="_localPosition">localPosition</param>
+        public PlayerActor(PlayerID _playerID, PlayerScriptableObject _sto, string _name, Vector3 _localPosition)
+               : base(_name, _localPosition)
+        {
+            playerID = _playerID;
+            scriptObj_ = _sto;
+            sr_ = go_.AddComponent<SpriteRenderer>();
+            sr_.sprite = scriptObj_.sprite;
+        }
+
+        /// <summary>
+        /// PlayerManager.Update()で呼び出される
+        /// </summary>
+        public override void OnUpdate()
+        {
+            Move();
+            Shot();
+        }
+        #endregion
+
+        #region Private Function
+        /// <summary>
+        /// 移動処理
+        /// </summary>
+        private void Move()
+        {
+            switch (playerID)
+            {
+                case PlayerID.Player1:
+                    if (Input.GetKey(KeyCode.UpArrow))
+                    {
+                        trans_.position += Vector3.up * scriptObj_.speed * Time.deltaTime * 100;
+                    }
+                    if (Input.GetKey(KeyCode.DownArrow))
+                    {
+                        trans_.position += Vector3.down * scriptObj_.speed * Time.deltaTime * 100;
+                    }
+                    if (Input.GetKey(KeyCode.RightArrow))
+                    {
+                        trans_.position += Vector3.right * scriptObj_.speed * Time.deltaTime * 100;
+                    }
+                    if (Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        trans_.position += Vector3.left * scriptObj_.speed * Time.deltaTime * 100;
+                    }
+                    break;
+
+                case PlayerID.Player2:
+                    /*
+                    if (Input.GetKey(KeyCode.UpArrow))
+                    {
+                        trans_.position += Vector3.up    * scriptObj_.speed * Time.deltaTime * 100;
+                    }
+                    if (Input.GetKey(KeyCode.DownArrow))
+                    {
+                        trans_.position += Vector3.down  * scriptObj_.speed * Time.deltaTime * 100;
+                    }
+                    if (Input.GetKey(KeyCode.RightArrow))
+                    {
+                        trans_.position += Vector3.right * scriptObj_.speed * Time.deltaTime * 100;
+                    }
+                    if (Input.GetKey(KeyCode.LeftArrow))
+                    {
+                        trans_.position += Vector3.left  * scriptObj_.speed * Time.deltaTime * 100;
+                    }
+                    */
+                    break;
+            }
+        }
+
+        private void Shot()
+        {
+            switch (playerID)
+            {
+                case PlayerID.Player1:
+                    if (Input.GetKey(KeyCode.Z))
+                    {
+                        Debug.Log(go_.name + ":shot!");
+                    }
+                    break;
+
+                case PlayerID.Player2:
+                    /*
+                    if (Input.GetKey(KeyCode.Z))
+                    {
+                        Debug.Log(go_.name + ":shot!");
+                    }
+                    */
+                    break;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Task.cs
@@ -0,0 +1,41 @@
+﻿namespace Ria
+{
+    /// <summary>
+    /// タスク
+    /// </summary>
+    /// <typeparam name="T">タスクの種類</typeparam>
+    internal sealed class Task<T>
+    {
+        #region member
+        public T item = default(T);
+        public Task<T> prev = null;
+        public Task<T> next = null;
+        #endregion
+
+        #region Main Function
+        /// <summary>
+        /// 接続処理
+        /// </summary>
+        /// <param name="prev">前のノード</param>
+        /// <param name="next">後のノード</param>
+        public void Attach(Task<T> prev, Task<T> next)
+        {
+            this.prev = prev;
+            this.next = next;
+            if (prev != null) { prev.next = this; }
+            if (next != null) { next.prev = this; }
+        }
+
+        /// <summary>
+        /// 切断処理
+        /// </summary>
+        public void Detach()
+        {
+            if (this.prev != null) { this.prev.next = this.next; }
+            if (this.next != null) { this.next.prev = this.prev; }
+            this.prev = null;
+            this.next = null;
+        }
+        #endregion
+    }
+}
